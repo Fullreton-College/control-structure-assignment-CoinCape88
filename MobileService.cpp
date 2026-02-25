11834fa99c9308439d9c5a6a705f94f167809bf4
Jonathan Hernandez <jonathanmhernandez20@gmail.com>
2026-02-24 17:47:22 -0800

    initial commit for MobileService.cpp

--- MobileService.cpp
+++ MobileService.cpp
@@ -1,2 +1,31 @@
 /* create the code yourself */
 /* Use Switch Statement */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Cost Calculator" << endl;
+    cout << "Enter the number of units purchased ";
+    int units;
+    cin >> units;
+
+    int discount;
+    if (units >= 100)
+    {discount = 0.50;}
+    else if (units >= 50 && units < 100)
+    {discount = 0.40;}
+    else if (units >= 20 && units < 50)
+    {discount = 0.30;}
+    else if (units >= 10 && units < 20)
+    {discount = 0.20;}
+
+    int price = 199;
+    double cost = price * units;
+    double totalCost = cost - (cost * discount);
+
+    cout << "The total cost for the amount of units purchased is $" << totalCost << "." << endl;
+
+    return 0;
+}
